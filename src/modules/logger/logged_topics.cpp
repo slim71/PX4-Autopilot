289e9b60614cb9581f111008d1615e0e561318a0
Matthias Grob <maetugr@gmail.com>
2023-12-04 11:21:15 +0100

    Differential Rover: PR fixes

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -56,7 +56,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("cellular_status", 200);
 	add_topic("commander_state");
 	add_topic("cpuload");
-	add_topic("differential_drive_setpoint", 100);
+	add_optional_topic("differential_drive_setpoint", 100);
 	add_optional_topic("external_ins_attitude");
 	add_optional_topic("external_ins_global_position");
 	add_optional_topic("external_ins_local_position");
